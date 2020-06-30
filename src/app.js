a5b3c91e1dd91880c25729a3ac6afbf583bf6c35
abuyako <abuyako@gmail.com>
2020-06-30 15:19:09 +0300

    add className for the container

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
   class App extends React.Component {
       render() {
           return (
-              <div>
+              <div className={styles.container}>
                    <Cards />
                    <CountryPicker />
                    <Chart />

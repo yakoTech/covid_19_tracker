71d05509f66a596c7a140893747cbc474211341b
abuyako <abuyako@gmail.com>
2020-06-30 15:05:46 +0300

    exporting the components file into the new index.js file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
   import React from 'react';
 
+//   import Cards from './components/Cards/Cards';
+//   import Chart from './components/Chart/Chart';
+//   import CountryPicker from './components/CountryPicker/CountryPicker ';
+
+  import { Cards, Chart, CountryPicker } from './components';
   class App extends React.Component {
       render() {
           return (

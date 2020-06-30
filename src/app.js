c0e30f7e8421f15c6e8897c8dccffc2b7b841f13
abuyako <abuyako@gmail.com>
2020-06-30 15:15:21 +0300

    add App.module.css

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
   import React from 'react';
 
-//   import Cards from './components/Cards/Cards';
-//   import Chart from './components/Chart/Chart';
-//   import CountryPicker from './components/CountryPicker/CountryPicker ';
-
   import { Cards, Chart, CountryPicker } from './components';
+  import styles from './App.module.css';
+
   class App extends React.Component {
       render() {
           return (
               <div>
-                  <h1>App</h1>
+                   <Cards />
+                   <CountryPicker />
+                   <Chart />
               </div>
           )
       }
